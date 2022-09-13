9dde7e5a288cebfdc8e415e5d23b944efcf8807b
Robert Gudat <robert.gudat@hutter-unger.de>
2022-09-13 11:37:33 +0200

    Added mailer service.

---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import { SSLApp, App, TemplatedApp, WebSocket, us_listen_socket_close } from "uws";
+import sendmail from 'sendmail';
+import * as fs from "fs";
+
+export default class MailService {
+
+    app: TemplatedApp;
+    mailer: SendMailFn;
+
+    constructor(
+        public default_sender_name: string,
+        public default_sender_email: string,
+        ssl = true,
+        key_file_name: string = 'mail.key',
+        cert_file_name: string = 'server.crt',
+        public user: string = '',
+        public password: string = '',
+        public dkim: string = 'dkim_private.pem',
+        public dkim_format: BufferEncoding = 'utf-8',
+        public key_selector: string = 'mails',
+        public maxPayload: number = 256 * 1024) {
+
+        if (ssl) {
+            this.app = SSLApp({
+                key_file_name,
+                cert_file_name
+            });
+        } else {
+            this.app = App({});
+        }
+
+        let aborted = () => { };
+
+        this.app.ws('/mail', {
+            idleTimeout: 0,
+            maxPayloadLength: maxPayload,
+            message: this.sendMail,
+
+            upgrade: (res, req, context) => {
+                res.onAborted(aborted);
+
+                if (this.user !== '' && req.getHeader('user') !== this.user ||
+                    this.password !== '' && req.getHeader('password') !== this.password) {
+                    return res.writeStatus('401 Unauthorized')
+                }
+
+                res.upgrade({},
+                    req.getHeader('sec-websocket-key'),
+                    req.getHeader('sec-websocket-protocol'),
+                    req.getHeader('sec-websocket-extensions'),
+                    context);
+            },
+        });
+    }
+
+    listen(host: string, port: number): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.app.listen(host, port, (listen_socket) => {
+                if (listen_socket) return resolve(true);
+                reject(false);
+            });
+        });
+    }
+
+    sendMail(ws: WebSocket, message: ArrayBuffer, _isBinary: boolean) {
+        let data = JSON.parse(Buffer.from(message).toString());
+        this.mail(
+            data.from ?? this.default_sender_email,
+            data.sender ?? this.default_sender_name,
+            data.to,
+            data.replyTo,
+            data.subject,
+            data.html)
+            .then(result => {
+                try { ws.send(data.id ?? 0 + result); } catch (err) { }
+            })
+            .catch(err => {
+                try { ws.send(data.id ?? 0 + err.message); } catch (err) { }
+            });
+    }
+
+    async mail(from: string, sender: string, to: string, replyTo: string, subject: string, html: string) {
+        return new Promise((resolve: (domain: string) => void, reject: (err: Error) => void) => {
+            if (!this.mailer)
+                this.mailer = sendmail({
+                    silent: true,
+                    dkim: {
+                        privateKey: fs.readFileSync(this.dkim, this.dkim_format),
+                        keySelector: this.key_selector
+                    }
+                });
+
+            this.mailer({ from, sender, to, replyTo, subject, html }, (err, domain) => {
+                if (err) return reject(err);
+                resolve(domain);
+            });
+        })
+    }
+}
+
+type SendMailFn = ((mail: sendmail.MailInput, callback: (err: Error, domain: string) => void) => void)
